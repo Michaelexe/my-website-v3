739963e25f17de819ddd2adf29107ce61a324f83
Michaelexe <michaelispahani@gmail.com>
2022-01-30 00:28:42 +0330

    added fancy background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
+import WAVES from "vanta/dist/vanta.waves.min";
 
 import "./App.css";
 import logo from "./assets/logo.png";
@@ -11,12 +12,36 @@ import SkillsPage from "./components/SkillsPage";
 import SocialLinks from "./components/SocialLinks/index.js";
 
 function App() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        WAVES({
+          el: document.body,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: 0x011627,
+          zoom: 1.18,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   const appearOptions = {
     threshold: 0,
     rootMargin: "0px 0px -100px 0px",
   };
 
-  let navbar = useRef();
+  const navbar = useRef();
   const skillsContainer = useRef();
   const socialLinks = useRef();
   const jumpLinks = useRef();
@@ -44,14 +69,21 @@ function App() {
     appearOnScroll.observe(skillsContainerTarget);
 
     var lastScrollTop = 0;
-    window.addEventListener("scroll", () => {
-      let scrollTop = window.scrollY || document.documentElement.scrollTop;
+    let root = document.querySelector("#root");
+    root.addEventListener("scroll", () => {
+      let scrollTop = root.scrollTop;
       if (scrollTop > lastScrollTop) {
         navbar.current.style.top = "-10vh";
       } else {
         navbar.current.style.top = "0";
       }
       lastScrollTop = scrollTop;
+
+      if (scrollTop > 500) {
+        navbar.current.style.backgroundColor = "rgba(1, 22, 39, 0.95)";
+      } else {
+        navbar.current.style.backgroundColor = "";
+      }
     });
 
     return () => {
@@ -61,8 +93,10 @@ function App() {
       if (skillsContainerTarget)
         appearOnScroll.unobserve(skillsContainerTarget);
 
-      window.removeEventListener("scroll", () => {
-        let scrollTop = window.scrollY || document.documentElement.scrollTop;
+      let root = document.querySelector("#root");
+
+      root.removeEventListener("scroll", () => {
+        let scrollTop = root.scrollY || document.documentElement.scrollTop;
         if (scrollTop > lastScrollTop) {
           navbar.style.top = "-10vh";
         } else {
