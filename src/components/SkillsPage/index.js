b14685138dc934327bfbbe04dc2ca039ba768be4
michaelexe <michaelispahani@gmail.com>
2024-04-28 00:42:30 -0400

    bug fix

--- src/components/SkillsPage/index.js
+++ src/components/SkillsPage/index.js
@@ -21,6 +21,144 @@ import cssSVG from "./assets/css.svg";
 import typescriptSVG from "./assets/typescript.svg";
 
 function SkillsPage() {
+  if (window.innerWidth < 500) {
+    return (
+      <section className="skills">
+        <div className="container">
+          <h1 className="HTMLTags">{"<skills>"}</h1>
+          <div className="honeycomb-grid">
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={pythonSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={javascriptSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={cppSVG} alt="" />
+              </div>
+            </div>
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={javaSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={dartSVG} alt="" />
+              </div>
+            </div>
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={typescriptSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={reduxSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={flaskSVG} alt="" />
+              </div>
+            </div>
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={nodejsSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={postgresqlSVG} alt="" />
+              </div>
+            </div>
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={reactSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={amazonSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={azureSVG} alt="" />
+              </div>
+            </div>
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={githubSVG} alt="" />
+              </div>
+
+              <div className="hexagon">
+                <img src={htmlSVG} alt="" />
+              </div>
+            </div>
+          </div>
+          <h1 className="HTMLTags" style={{ marginLeft: "auto" }}>
+            {"</skills>"}
+          </h1>
+        </div>
+      </section>
+    );
+  }
+
+  if (window.innerWidth < 1000) {
+    return (
+      <section className="skills">
+        <div className="container">
+          <h1 className="HTMLTags">{"<skills>"}</h1>
+          <div className="honeycomb-grid">
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={pythonSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={javascriptSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={cppSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={javaSVG} alt="" />
+              </div>
+            </div>
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={dartSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={typescriptSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={reduxSVG} alt="" />
+              </div>
+            </div>
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={flaskSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={nodejsSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={postgresqlSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={reactSVG} alt="" />
+              </div>
+            </div>
+            <div className="honeycomb-row">
+              <div className="hexagon">
+                <img src={amazonSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={azureSVG} alt="" />
+              </div>
+              <div className="hexagon">
+                <img src={githubSVG} alt="" />
+              </div>
+            </div>
+          </div>
+          <h1 className="HTMLTags" style={{ marginLeft: "auto" }}>
+            {"</skills>"}
+          </h1>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className="skills">
       <div className="container">
